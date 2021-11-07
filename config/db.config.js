6ffc273418291ba9c876e95f48220fefa34cf788
Mark33Mark <mark@watsonised.com>
2021-11-07 11:12:58 +1100

    Fixed a CSS media issue with the landscape device trigger.

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 
 // mySQL connection parameters
 module.exports = {
-        HOST: "##############",
-        USER: "##############",
-        PASSWORD: "###########",
-        DATABASE: "###########"
-    };
+    HOST: "us-cdbr-east-04.cleardb.com",
+    USER: "b01cb4f1d8dd12",
+    PASSWORD: "e0bb1d74",
+    DATABASE: "heroku_251d454de09f27b"
+};
 
