24f0411cdd6ece9f09fd331662ca7e5eff002f86
Mark33Mark <mark@watsonised.com>
2021-11-07 17:56:42 +1100

    Updated some CSS again to better manage Landscape.

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,9 @@
 
 // mySQL connection parameters
 module.exports = {
-    HOST: "us-cdbr-east-04.cleardb.com",
-    USER: "b01cb4f1d8dd12",
-    PASSWORD: "e0bb1d74",
-    DATABASE: "heroku_251d454de09f27b"
+    HOST: "###############",
+    USER: "###############",
+    PASSWORD: "###############",
+    DATABASE: "###############"
 };
 
