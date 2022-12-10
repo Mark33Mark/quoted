708fea66ecc9c902bd86512527cf23c25fcf45b1
Mark33Mark <mark@watsonised.com>
2022-12-10 20:10:10 +1100

    [feature] Refactored as serverless with Netlify functions.

---
+++ netlify/functions/random.js
@@ -0,0 +1,55 @@
+const mysql = require( "mysql2" );
+const { randomQuote } = require("../../models/quotes.model");
+require('dotenv').config();
+
+// Create a connection to the database
+const connection = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+const sql_query = "SELECT * FROM quotes ORDER BY RAND() LIMIT 1";
+
+exports.handler = (event, context, callback) => {
+
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  try {
+
+    connection.getConnection(function(err, connected) {
+
+      connected.execute( sql_query, (error, results) => {
+
+      if (error){ 
+        console.log('calling callback with error')
+        callback(error);
+      } else {
+
+        console.log("Quote of the day: ", results[0]);
+
+        callback( null, {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(results[0])
+        })
+      }
+    })
+  })
+} catch (e) {
+    console.log('There is an error in communicating with the Quotes database: ', e);
+  }
+}
+
+
+// exports.handler = async (event) => {
+//   console.log({"message": "Welcome to the quotes page."});
+//   return {
+//       statusCode: 200,
+//       body: JSON.stringify({message: 'Welcome to the quotes page.'})
+//   }
+// }
