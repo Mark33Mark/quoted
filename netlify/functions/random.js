14338a25ffef4a269b4fbf733f4ba9e707ad759b
Mark33Mark <mark@watsonised.com>
2024-06-17 08:15:17 +1000

    [REFACTOR] trying to resolve issue with current version of mysql2

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -1,50 +1,57 @@
-const db_connect = require( "./utils/db_connection.js" );
+const db_connect = require('./utils/db_connection.js');
 
-const sql_query = "SELECT * FROM quotes ORDER BY RAND() LIMIT 1";
-const sql_count_database_length = "SELECT (SELECT COUNT(1) FROM quotes) AS database_length";
+const sql_query = 'SELECT * FROM quotes ORDER BY RAND() LIMIT 1';
+const sql_count_database_length =
+	'SELECT (SELECT COUNT(1) FROM quotes) AS database_length';
 
 exports.handler = (event, context, callback) => {
+	context.callbackWaitsForEmptyEventLoop = false;
+	const clientMessage = JSON.parse(event.body);
 
-  context.callbackWaitsForEmptyEventLoop = false;
-  const clientMessage = JSON.parse(event.body);
+	try {
+		db_connect.getConnection(function (err, connected) {
+			connected.execute(sql_query, (error, results) => {
+				if (error instanceof Error) {
+					console.log('calling callback with error: ', err);
+					callback(error);
+					return;
+				} else {
+					// as connection is available, run 2nd query to get number of quotes available
+					connected.execute(sql_count_database_length, (error, counter) => {
+						if (error instanceof Error) {
+							console.log('calling callback with error: ', err);
+							callback(error);
+							return;
+						} else {
+							const mergedObject = {
+								...clientMessage,
+								...results[0],
+								...counter[0],
+							};
+							console.log('Quote of the day: ', mergedObject);
 
-  try {
+							callback(null, {
+								statusCode: 200,
+								headers: {
+									'Access-Control-Allow-Origin': '*',
+									'Content-Type': 'application/json',
+								},
+								body: JSON.stringify(mergedObject),
+							});
+						}
+					});
+				}
+				db_connect.releaseConnection(connected);
+			});
 
-    db_connect.getConnection(function(err, connected) {
-
-      connected.execute( sql_query, (error, results) => {
-
-      if ( error instanceof Error ){ 
-
-        console.log('calling callback with error: ', err)
-        callback(error);
-        return;
-
-      } else {
-
-        // as connection is available, run 2nd query to get number of quotes available
-        connected.execute( sql_count_database_length, ( error, counter) => {
-        
-        const mergedObject = { ...clientMessage, ...results[0], ...counter[0]};
-        console.log("Quote of the day: ", mergedObject);
-
-          callback( null, {
-              statusCode: 200,
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(mergedObject)
-            })
-          })
-          connected.release();
-        }
-      })
-
-    if(err) { console.log('db_connect error = ', err); }
-
-  })
-} catch (e) {
-    console.log('There is a problem communicating with the Quotes database: ', e);
-  }
-}
+			if (err) {
+				console.log('db_connect error = ', err);
+			}
+		});
+	} catch (e) {
+		console.log(
+			'There is a problem communicating with the Quotes database: ',
+			e
+		);
+	}
+};
--- netlify/functions/utils/db_connection.js
+++ netlify/functions/utils/db_connection.js
@@ -1,19 +1,21 @@
 const mysql = require('mysql2');
 
+const connectionSettings = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+}
+
 const connection = 
   // Create a connection to the database
-  mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-  });
+  mysql.createPool(connectionSettings);
 
 module.exports = connection;
