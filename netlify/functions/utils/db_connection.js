1ff062c32cb486e250ffbd244db8cb31e64cb364
Mark33Mark <mark@watsonised.com>
2024-06-16 19:02:15 +1000

    [REVERT] Reverting mysql2 version to earlier as current version not working

--- netlify/functions/utils/db_connection.js
+++ netlify/functions/utils/db_connection.js
@@ -6,7 +6,8 @@ const connection =
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: 3306
   });
 
 module.exports = connection;
