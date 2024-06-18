a3cc5be7a70a8e15fa47f39bec9eb35528aa6a0c
Mark33Mark <mark@watsonised.com>
2024-06-18 21:49:03 +1000

    [UPDATE] Refactoring to get mysql2 promise to work in prod on netlify.

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -5,8 +5,6 @@ const sql_count_database_length =
 	'SELECT (SELECT COUNT(1) FROM quotes) AS database_length';
 
 exports.handler = async (event) => {
-	
-	let mergedObject;
 	const clientMessage = JSON.parse(event.body);
 
 	try {
@@ -14,24 +12,25 @@ exports.handler = async (event) => {
 		const results = await connected.execute(sql_query);
 		const counter = await connected.execute(sql_count_database_length);
 
-		mergedObject = {
+		const mergedObject = {
 			...clientMessage,
 			...results[0][0],
 			...counter[0][0],
 		};
 		console.log('Quote of the day: ', mergedObject);
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(mergedObject),
+		};
 	} catch (error) {
 		console.log(
 			'There is a problem communicating with the Quotes database:\n',
 			error
 		);
 	}
-	return {
-		statusCode: 200,
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(mergedObject),
-	};
 };
