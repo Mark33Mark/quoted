d4b2414a0e9e6c9419a0db216e1384e504d44ce4
Mark33Mark <mark@watsonised.com>
2024-06-18 21:32:50 +1000

    [UPDATE] Refactored to see if current version of mysql2 works on prod

--- netlify/functions/random.js
+++ netlify/functions/random.js
@@ -4,37 +4,34 @@ const sql_query = 'SELECT * FROM quotes ORDER BY RAND() LIMIT 1';
 const sql_count_database_length =
 	'SELECT (SELECT COUNT(1) FROM quotes) AS database_length';
 
-exports.handler = async (event, context) => {
-	context.callbackWaitsForEmptyEventLoop = false;
+exports.handler = async (event) => {
+	
+	let mergedObject;
 	const clientMessage = JSON.parse(event.body);
 
 	try {
 		const connected = await connection.getConnection();
 		const results = await connected.execute(sql_query);
-		const counter = await connected.execute(sql_count_database_length)
+		const counter = await connected.execute(sql_count_database_length);
 
-		console.log( 'clientMessage = ', clientMessage)
-		console.log( 'results = ', results[0][0])
-		console.log( 'counter = ', counter[0][0])
-				const mergedObject = {
-					...clientMessage,
-					...results[0][0],
-					...counter[0][0],
-				};
-				console.log('Quote of the day: ', mergedObject);
-
-				return {
-					statusCode: 200,
-					headers: {
-						'Access-Control-Allow-Origin': '*',
-						'Content-Type': 'application/json',
-					},
-					body: JSON.stringify(mergedObject),
-				};
+		mergedObject = {
+			...clientMessage,
+			...results[0][0],
+			...counter[0][0],
+		};
+		console.log('Quote of the day: ', mergedObject);
 	} catch (error) {
 		console.log(
 			'There is a problem communicating with the Quotes database:\n',
 			error
 		);
 	}
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(mergedObject),
+	};
 };
