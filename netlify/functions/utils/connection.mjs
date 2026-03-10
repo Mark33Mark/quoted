52a780fd9fe5ba5036ba9b1198e0ae999fe0d9c6
Mark33Mark <mark@watsonised.me>
2026-03-10 14:35:21 +1100

    [BUG] updated mysql2 connection configuration

--- netlify/functions/utils/connection.mjs
+++ netlify/functions/utils/connection.mjs
@@ -7,12 +7,11 @@ const connectionSettings = {
   database: process.env.DATABASE,
   password: process.env.PASSWORD,
   charset: 'utf8mb4',     // critical setting for emojis to be accepted
-  maxIdle: 1,             // default value is the same as `connectionLimit`
   connectionLimit: 2,     // do not exceed 5, for low traffic 1-2 is optimum.
-  waitForConnections: true,
+  maxIdle: 1,             // default value is the same as `connectionLimit`
+  idleTimeout: 30000,    // default value 60000
   enableKeepAlive: true, // proactively checks connection health
   keepAliveInitialDelay: 20000,
-  idleTimeout: 30000,    // default value 60000
 }
 
 export const connection =
